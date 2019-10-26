05db9029fde612255cb31d5c90bb266f4940029c
Jay Dickson <jaydickson02@gmail.com>
2019-10-26 19:59:05 +1100

    Text and Clock animation

---
+++ components/textAnimation.js
@@ -0,0 +1,49 @@
+
+
+import React from 'react';
+
+//Material
+
+
+let words = ['Javascript', 'Next.js', 'Python', 'React', 'Express', 'Node.js', 'MongoDB', 'Docker'];
+let counter = 0;
+
+class TextAnimation extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        word: words[counter]
+      };
+    }
+    
+    componentDidMount() {
+      this.intervalID = setInterval(
+        () => this.add(),
+        2500
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+    add() {
+        if(counter < words.length - 1){
+            counter += 1;
+        } else {
+            counter = 0;
+        }
+        
+      this.setState({
+        word: words[counter]
+      });
+    }
+    render() {
+      return (
+          <div>
+         {this.state.word}
+         </div>
+      );
+    }
+  }
+
+  export default TextAnimation;
