5bc2378745ed8347b2cc33830d57101eb3e0f408
Jay Dickson <jaydickson02@gmail.com>
2019-10-26 13:43:23 +1100

    Cleared out testing code

--- pages/blog.js
+++ pages/blog.js
@@ -1,16 +1,25 @@
 //Material
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
 //Components
 import PostBoard from "../components/blog/postBoard"
 import Navbar from "../components/navbar"
 //Next
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(2,1)
+    },
+  }));
+
 let blog = () => {
-    
+
+    const classes = useStyles();
+
     return(
         <div>
-            <Navbar />
-            <h1>Blog!!!</h1>
+            <Navbar page={1}/>
             <PostBoard />
         </div>
     )
