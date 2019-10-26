dcbdbca54ee75712e2660985c7f1f187d6984f5c
Jay Dickson <jaydickson02@gmail.com>
2019-10-26 19:59:35 +1100

    New page

---
+++ pages/passwordreset.js
@@ -0,0 +1,14 @@
+
+
+let resetpass = () => {
+
+    return(
+        <div>
+            <h1>
+                Under Constuction
+            </h1>
+        </div>
+    )
+}
+
+export default resetpass;
