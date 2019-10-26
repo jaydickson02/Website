4007a9ed1e501c1622c9a3dd897f8e8559bc6a6c
Jay Dickson <jaydickson02@gmail.com>
2019-10-26 19:59:19 +1100

    Main page redesign

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import React from 'react';
+
 //Material UI Components
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+
 //Components
 import Navbar from "../components/navbar"
 import Head from "../components/head"
 import Drawer from "../components/drawer"
 import Social from "../components/socialDisplay"
+import Clock from "../components/clock"
+import TextAnimation from "../components/textAnimation"
 
 //Material
 import Paper from '@material-ui/core/Paper';
@@ -25,19 +30,22 @@ const useStyles = makeStyles(theme => ({
       
     },
     headPlate: {
-        padding: theme.spacing(10, 2)
+        padding: theme.spacing(8, 2)
+        
     },
     wrapper:{
-        height: '100vh'
+        height: '100vh',
+        backgroundImage: "url('/backgrounds/dot-grid.png')"
     },
     socialPadding: {
-        padding: theme.spacing(2, 2)
+        padding: theme.spacing(0, 2)
     }
   }));
 
-let index = () => {
 
-    const classes = useStyles();
+let index = () => {
+    const classes = useStyles()
+    
 
 return(
     <div>
@@ -45,37 +53,42 @@ return(
         <Box className={classes.wrapper}>
         <Navbar />
 
-        <Grid container alignItems="center" spacing={4}>
+        <Grid container alignItems="center" spacing={1}>
             <Grid item xs={12}>
                 <Box className={classes.headPlate}>
                         <Typography variant='h1' align='center' gutterBottom>
                             Jay Dickson
                         </Typography>
-                        <Typography variant='h4' align='center' gutterBottom>
-                            The website of champions!
-                        </Typography>
                         <Typography variant='h5' align='center' gutterBottom>
+                            Developing with
+                        </Typography>
+                        <Typography variant='h3' align='center' gutterBottom>
+                           <b><TextAnimation /></b>
+                        </Typography>
+                        <Typography variant='h6' align='center' gutterBottom>
                             Check out my projects!
                         </Typography>
+                        
+                        
+
+                        
                 </Box>
             </Grid>
 
             <Grid item xs={12}>
             <Typography variant='h1' align='center' gutterBottom>
                 <Box component="span" className={classes.socialPadding}>
-                    <Link href='http://github.com/jaydickson02'>
-                        <a>
-                            <img src="static/socialLogos/GitHub-Mark-120px-plus.png" width="64px" height="64px"/>
+                
+                        <a href='http://github.com/jaydickson02'>
+                            <img src="/socialLogos/GitHub-Mark-120px-plus.png" width="64px" height="64px"/>
                         </a>
-                    </Link>
+                    
                 </Box>
                 
                 <Box component="span" className={classes.socialPadding}>
-                    <Link href='https://twitter.com/Jaymonkey01'>
-                        <a>
-                            <img src="static/socialLogos/Twitter_Logo_Blue.png" width="64px" height="64px"/>
+                        <a href='https://twitter.com/Jaymonkey01'>
+                            <img src="/socialLogos/Twitter_Logo_Blue.png" width="64px" height="64px"/>
                         </a>
-                    </Link>
                 </Box>
             </Typography>
             </Grid>
@@ -83,25 +96,13 @@ return(
         </Grid>
         </Box>
 
+        
+
         <Grid container alignItems="center" spacing={4}>
             <Grid item xs={12}>
                 <Paper className={classes.root}>
                     <Typography variant='h1' align='center' gutterBottom>
-                        Testing 123
-                    </Typography>
-                </Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.root}>
-                    <Typography variant='h1' align='center' gutterBottom>
-                        Testing 123
-                    </Typography>
-                </Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.root}>
-                    <Typography variant='h1' align='center' gutterBottom>
-                        Testing 123
+                        Developers
                     </Typography>
                 </Paper>
             </Grid>
@@ -109,6 +110,7 @@ return(
         
     </div>
 )
+
 
 }
 
