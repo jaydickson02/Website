fd51a8fc0e3d6562b8a919a0db3f993117679f16
Jay Dickson <jaydickson02@gmail.com>
2019-10-26 22:38:47 +1100

    Added Under construction note

--- pages/blogpost.js
+++ pages/blogpost.js
@@ -2,13 +2,50 @@
 //Make a variable html path
 //TODO: Add get initial props
 
+//Material
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Navbar from "../components/navbar"
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(20, 2),
+      
+    },
+    headPlate: {
+        padding: theme.spacing(20, 2)
+        
+    },
+    wrapper:{
+        height: '100vh'
+    },
+    socialPadding: {
+        padding: theme.spacing(0, 2)
+    }
+  }));
+
 let blogpost = (props) => {
+
+    const classes = useStyles()
 
     //content = props.content;
 
     return(
         <div>
-            <h1>This will be a blog post</h1>
+            <Box className={classes.wrapper}>
+            <Navbar />
+            <Grid container alignItems="center" spacing={1}>
+                <Grid item xs={12}>
+                    <Box className={classes.headPlate}>
+                            <Typography variant='h1' align='center' gutterBottom>
+                                Under Construction
+                            </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+            </Box>
         </div>
     )
 }
