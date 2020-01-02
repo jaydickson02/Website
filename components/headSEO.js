9c970a108e25b55ac9b8b527ce84e00dcdbf7f8a
Jay Dickson <jaydickson02@gmail.com>
2020-01-03 02:06:59 +1100

    Added Favicon

--- components/headSEO.js
+++ components/headSEO.js
@@ -22,6 +22,10 @@ const Meta = (props) => (
 <link rel="canonical" href="" />
 <script type="text/javascript" src="" ></script>
 
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
 
 
 </Head>
