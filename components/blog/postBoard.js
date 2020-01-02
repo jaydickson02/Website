7651414bfa1ccf7daeae9ac2cd530cab447ab768
Jay Dickson <jaydickson02@gmail.com>
2020-01-02 22:32:10 +1100

    Cleaned up

--- components/blog/postBoard.js
+++ components/blog/postBoard.js
@@ -4,28 +4,18 @@ import PostSummary from "./postSummary"
 //Material
 import Grid from '@material-ui/core/Grid';
 
-let testData = [
-
-    {title: "How to code", body: "This is how you code. Its basically magic.", author: "Jay Dickson"},
-    {title: "Why are you here?", body: "Probably something to do with teleportation?", author: "Billy Topnotch"},
-    {title: "Pretty fly for a white guy", body: "But is he pretty white for a fly guy?", author: "Ben Farland"}
-
-]
 
 
 let postBoard = (props) => {
-    console.log(props.posts);
-    let posts;
-    if(props.posts.length > 0){
-        console.log('hey') 
-        posts = props.data.map(blog => 
-    <Grid item xs={12}> 
-        <PostSummary content = {blog.title} /> 
+    
+    let posts = props.posts.map
+
+    (post => 
+     <Grid key={post.title} item xs={12}> 
+        <PostSummary content = {post} /> 
     </Grid>
     )
-    } else {
-        posts = "No Blog Posts Found";
-    }
+    
 
     return(
 
@@ -35,13 +25,6 @@ let postBoard = (props) => {
             </Grid>
         </div>
     )
-}
-
-postBoard.getInitialProps = async () => {
-    const res = await fetch('http://db.originalone.cloud/api/answers/all');
-    const data = await res.json();
-
-  return{posts: data};
 }
 
 export default postBoard;
