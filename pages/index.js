663c364484c9f27cfd31cc9e56100fe64a1910b2
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2021-01-04 18:58:20 +1100

    Updated fields in Nav and Index

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,11 @@ return(
     <hr />
 
         <Alert variant={'primary'}>
-            This is the landing page for DropIT. Welcome. We're currently under construction. Feel free to have a look around!
+            This is the landing page for Jay's Website. Welcome. We're currently under construction. Feel free to have a look around!
         </Alert>
 
     <hr />
-
-        <Jumbotron>
-            <h1>DropIt</h1>
-            <p>
-                We're working to build a clean, minimal and highly functional system that's capable of managing all the needs of an IT Department with respect to hardware. Soloutions are available for inventory organisation, tracking repairs and managing users.
-            </p>
-
-            <p>
-                Click below to check out the devices page. 
-            </p>
-                
-            
-            <p>
-                <Button href="/devices" variant="primary">Go</Button>
-            </p>
-        </Jumbotron>
+        
     </Container>
 </Layout>
 )
--- shared/navbar.js
+++ shared/navbar.js
@@ -11,15 +11,15 @@ let navbar = (props) => {
     return(
         
         <Navbar varient="light" expand="sm" style={{paddingTop: '22px', paddingBottom:'22px'}}>
-        <Navbar.Brand href="/">DropIt</Navbar.Brand>
+        <Navbar.Brand href="/">Jay Dickson</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="mr-auto"  activeKey={props.activeLink}>
-            <Nav.Link href="/devices">Devices</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
+            <Nav.Link href="/devices">Projects</Nav.Link>
+            <Nav.Link href="/users">Notes</Nav.Link>
         </Nav>
           <Nav className="justify-content-end"  activeKey={props.activeLink}>
-            <Nav.Link href="/settings">Settings</Nav.Link>
+            <Nav.Link href="/settings">About</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
