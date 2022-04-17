468dbe3e27731c9b90dbadc7a45d1e11356cb9e0
Jay Dickson <jaydickson02@gmail.com>
2022-04-17 16:31:09 +1000

    Potential Fix for Font Awesome Bug

--- shared/layout.js
+++ shared/layout.js
@@ -4,6 +4,13 @@ import Head from './head';
 import Container from 'react-bootstrap/Container'
 import Footer from './footer'
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+
 let layout = (props) => {
     
 
