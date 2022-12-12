d3419dc123875de5604449ed6d29d5a07ba14591
Jay Dickson <jaydickson02@gmail.com>
2022-12-12 12:28:23 +1100

    Added Analytics

--- shared/head.js
+++ shared/head.js
@@ -1,17 +1,29 @@
 
 
 let head = () => {
-    return(
-    <div>
-        <head>
-        <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-                integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-                crossOrigin="anonymous"
-            />
-        </head>
-    </div>
+    return (
+        <div>
+            <head>
+                <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                    crossOrigin="anonymous"
+                />
+            </head>
+
+
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WX88YX6P8X"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+
+                gtag('config', 'G-WX88YX6P8X');
+            </script>
+
+        </div>
     )
 
 }
