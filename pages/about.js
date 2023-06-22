3e0180f4862fc89e1417fa4f217f82c8bde55221
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2023-06-22 15:30:57 +1000

    Relocated Images

--- pages/about.js
+++ pages/about.js
@@ -11,16 +11,8 @@ const about = () => {
             <div class=" p-10 rounded-2xl">
 
             <h1>Hi, I'm Jay Dickson.</h1>
-            <div class="flex justify-center p-10">
-            <div class="w-11/12">
-                <div class="grid grid-cols-5 gap-2">
-                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/jay-dickson.webp"} alt="Profile" width="360px" height="450px" />
-                <Image class="rounded-3xl transform rotate-3 scale-75" src={"/aboutImages/cook-jay.webp"} alt="Profile" width="360px" height="450px" />
-                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/run-jay.webp"} alt="Profile" width="360px" height="450px" />
-                <Image class="rounded-3xl transform rotate-3 scale-75" src={"/aboutImages/lm-jay.webp"} alt="Profile" width="360px" height="450px" />
-                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/wine-jay.webp"} alt="Profile" width="360px" height="450px" />
-                </div>
-                </div>
+            <div class="flex justify-center p-4">
+
             </div>
                 <p>
                     I am currently a student of <strong>Aerospace Engineering at RMIT</strong>, and I couldn't be more excited about the field I've chosen to study.
@@ -35,6 +27,18 @@ const about = () => {
                     Currently, I am also working as a <strong>Digital Technology Officer</strong> at a Primary School, where I am responsible for overseeing the integration of technology into the classroom.
                 </p>
             </div>
+            <div class="flex justify-center">
+            <div class="w-11/12">
+                <div class="grid grid-cols-5 gap-2">
+                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/jay-dickson.webp"} alt="Profile" width="360px" height="450px" />
+                <Image class="rounded-3xl transform rotate-3 scale-75" src={"/aboutImages/cook-jay.webp"} alt="Profile" width="360px" height="450px" />
+                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/run-jay.webp"} alt="Profile" width="360px" height="450px" />
+                <Image class="rounded-3xl transform rotate-3 scale-75" src={"/aboutImages/lm-jay.webp"} alt="Profile" width="360px" height="450px" />
+                <Image class="rounded-3xl transform -rotate-3 scale-75" src={"/aboutImages/wine-jay.webp"} alt="Profile" width="360px" height="450px" />
+                </div>
+            </div>
+            </div>
+            
 
         </Layout>
     )
